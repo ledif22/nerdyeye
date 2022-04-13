24c48e75038cb2b111f12797e58885dbbd8e2b6e
ledif22 <you@example.com>
2022-04-13 14:58:35 +0100

    initial commit

---
+++ weekOne/app.js
@@ -0,0 +1,40 @@
+// class People{
+//     constructor(name, profession, age){
+//         this.name = name
+//         this.profession = profession
+//         this.age = age
+//     }
+// }
+
+// const person = new People ('John Doe', 'Developer', 50)
+// const person1 = new People ('Amaka', 'Backend Developer', 30)
+
+// console.log(person)
+// console.log(person1)
+
+// const animals = ['dog', 'cat', 'bird']
+
+// animals.unshift('snake')
+// console.log(animals)
+
+// function declaration
+// sayHi('Benson')
+// function sayHi (name){
+//     console.log(`Hi ${name}`)
+// }
+
+// sayHi('Johnson')
+
+//function expression
+// //Anonymous function
+// (()=> {
+//     console.log('Hello Node')
+// })()//immediately invoked function expression
+
+// Global variable in Node
+const direct = __dirname
+const filename= __filename
+
+console.log(direct)
+console.log(filename)
+
